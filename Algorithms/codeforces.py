967d49eb789c7a582b7859b7a7d29d1f618d970f
Destroyagony <147639560+Destroyagony@users.noreply.github.com>
2023-12-15 19:59:23 +0300

    codeforces

--- Algorithms/codeforces.py
+++ Algorithms/codeforces.py
@@ -1,27 +1,3 @@
-https://codeforces.com/problemset/problem/50/A
-solve1
-#print(eval(input().replace(' ','*'))//2)
-solve2
-#while True:
-    #size = input()
-    #dimensions = size.split(" ")
-    #try:
-        #length = int(dimensions[0])
-        #width = int(dimensions[1])
-    #except ValueError:
-       # pass
-    #else:
-        #break
- 
-#Find area
-#area = length * width
-#Find max dominoes
-#dominoes = int(area/2 - (area%2)/2)
- 
-#print final answer
-#print(dominoes)
-solve3
-#inp=[int(x) for x in input().split()]
-#M=inp[0]
-#N=inp[1]
-#print((M*N)//2)
+# https://codeforces.com/problemset/problem/546/A
+(k, n, w) = tuple(map(lambda x: int(x) if x != '' else None, input().split(" ")))
+print((lambda x: 0 if x <= 0 else x)(k*w*(w+1)//2-n))
